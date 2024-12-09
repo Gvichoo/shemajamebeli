68992112e0dfa90c3aafc5d5a4eeb55f49a2ae76
Gvichoo <saba.gvichiani27@gmail.com>
2024-12-09 10:05:36 +0400

    Shemajamebeli

---
+++ app/src/main/java/com/example/shemajamebeli/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.shemajamebeli
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shemajamebeli.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val listOfWords = mutableListOf<String>()
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.saveButton.setOnClickListener {
+            val enteredAnagram = binding.enterAnagram.text.toString()
+            if (enteredAnagram.isEmpty()){
+                Toast.makeText(this,"Enter a word!",Toast.LENGTH_LONG ).show()
+            }else{
+                listOfWords.add(enteredAnagram)
+                binding.enterAnagram.text.clear()
+            }
+
+        }
+
+        binding.clearButton.setOnClickListener {
+            binding.enterAnagram.text.clear()
+            binding.anagramsTextView.text = "Anagrams"
+            listOfWords.clear()
+            Toast.makeText(this,"Cleared!",Toast.LENGTH_LONG).show()
+        }
+        binding.outputButton.setOnClickListener {
+            val groupedAnagrams = listOfWords.groupBy { it.toCharArray().sorted().joinToString("") }
+            val result = groupedAnagrams.values.joinToString("\n") { it.toString() }
+            binding.anagramsTextView.text = result
+        }
+        binding.connectButton.setOnClickListener {
+            Toast.makeText(this,"Text:Hello i am hacker from tbc it academy - has been sent to other devices.",Toast.LENGTH_LONG).show()
+        }
+    }
+}
